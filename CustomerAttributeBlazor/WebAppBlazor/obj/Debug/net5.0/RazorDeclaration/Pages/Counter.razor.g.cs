5e7aece67c895cab38cff17132db7c11fdbea134
thuntndgh <thuntn.vndigitech@gmail.com>
2022-01-08 00:16:12 +0700

    clear debug

--- CustomerAttributeBlazor/WebAppBlazor/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ CustomerAttributeBlazor/WebAppBlazor/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace WebAppBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 1 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 2 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 3 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 4 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 5 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 6 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 7 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 8 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 9 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using WebAppBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
+#line 10 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\_Imports.razor"
 using WebAppBlazor.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using WebAppBlazor.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "D:\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\Pages\Counter.razor"
+#line 9 "D:\Digitech\projects\Project2\CustomerAttributeBlazor\CustomerAttributeBlazor\WebAppBlazor\Pages\Counter.razor"
        
     private int currentCount = 0;
 
